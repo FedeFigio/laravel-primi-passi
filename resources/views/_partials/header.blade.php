80a60e56d04b968f8237e0c475406679f1633ebd
Federico <federicofigini90@gmail.com>
2021-05-11 15:24:25 +0200

    finito

---
+++ resources/views/_partials/header.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <style>
+        header {
+            height: 100px;
+            background-color: red;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 50px;
+            text-transform: uppercase
+        }
+
+    </style>
+</head>
+<header>
+    questo e un header importato
+</header>
